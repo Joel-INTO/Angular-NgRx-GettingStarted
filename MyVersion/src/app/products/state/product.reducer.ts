9fa82168d7e6f47b38b39cc5eaffdd973034cb1e
Joel <joel.balmer@gmail.com>
2020-02-19 09:35:15 +0000

    End of module 5

---
+++ MyVersion/src/app/products/state/product.reducer.ts
@@ -0,0 +1,12 @@
+export const ProductReducer = (state, action) => {
+    switch (action.type) {
+        case 'TOGGLE_PRODUCT_CODE':
+            return {
+                ...state,
+                showProductCode: action.payload
+            };
+
+        default:
+            return state;
+    }
+};
